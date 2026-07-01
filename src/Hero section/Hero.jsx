ae0863d44156d555f8946fa5948bb7d5d2841567
vishwajeet <vwjchindariya473443@gmail.com>
2026-07-02 00:07:15 +0530

    renew

--- src/Hero section/Hero.jsx
+++ src/Hero section/Hero.jsx
@@ -12,18 +12,21 @@ const Hero=()=>{
            <br/> Fast reallable and hassle-free home services</h2>
            <button className="mt-6 border border-blue-500 text-blue-500
             px-5 py-2 rounded-full hover:bg-blue-500 hover:text-white "><FaMapMarkerAlt/>Track Repair</button>
-            <div className="mt-6 flex items-center gap-4">
-                <div className="flex gap-2">
+            {/* <div className="mt-6 flex items-center gap-4"> */}
+            <div className="mt-6 flex flex-col sm:flex-row items-center gap-4">
+                <div className="flex gap-2 justify-center">
                     <img src="w1.jpg" alt="images" className="w-12 h-12 rounded-full border-2 border-white" />
                      <img src="w2.jpg" alt="images" className="w-12 h-12 rounded-full border-2 border-white" />
                       <img src="w3.jpg" alt="images" className="w-12 h-12 rounded-full border-2 border-white" />
                        <img src="w4.jpg" alt="images" className="w-12 h-12 rounded-full border-2 border-white" />
                         </div>
                        <div>
-<h3 className="font-semibold text-lg">
+{/* <h3 className="font-semibold text-lg"> */}
+<h3 className="font-semibold text-base sm:text-lg text-center sm:text-left">
     Trusted by 10,000+ Customers
 </h3> 
-<p className="flex items-center gap-2 text-yellow-500">
+{/* <p className="flex items-center gap-2 text-yellow-500"> */}
+<p className="flex flex-col sm:flex-row items-center gap-2 text-yellow-500">
     ***** <samp className="text-gray-700font-medium">4.8(1200+ Reviews)</samp>
 </p>
 
